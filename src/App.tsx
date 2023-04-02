7bae06eca58718e8b5954df21fac854f8fe266c1
diennb0 <118904886+diennb0@users.noreply.github.com>
2023-04-03 03:52:41 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import React, { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
 // import "./App.css";
-import { Route, Routes, useParams } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 //Layout
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
-
 import Dashboard from "./admin/Dashboard";
-//SanPham
+//PRODUCTS OR API
+import {
+  addProduct,
+  deleteProduct,
+  getAllProduct,
+  updateProduct,
+} from "./api/product";
 import ProductManagement from "./admin/product/productManagement";
 import AddProduct from "./admin/product/AddProduct";
 import UpdateProduct from "./admin/product/UpdateProduct";
 //DangNhap
-import SignUpPage from "./pages/SignUpPage";
-import SignInPage from "./pages/SignInPage";
+
 //Layout
 import ClientLayout from "./layout/ClientLayout";
 import AdminLayout from "./layout/AdminLayout";
@@ -22,24 +25,17 @@ import AdminLayout from "./layout/AdminLayout";
 import { ICate, IProduct, IUser } from "./interface/Interface";
 //User
 import { addUser, signin } from "./api/user";
-//router
-import {
-  addProduct,
-  deleteProduct,
-  getAllProduct,
-  getProductId,
-  updateProduct,
-} from "./api/product";
+//ROUTER or API
+
+import { useNavigate } from "react-router-dom";
+//CATEGORY or API
 import {
   createCate,
   deleteCate,
   getAllCategory,
   updateCate,
 } from "./api/category";
-import { useNavigate } from "react-router-dom";
-//Danhmuc
 import AddCategory from "./admin/category/addCategory";
-
 import CategoryManagement from "./admin/category/categoryManagement";
 import UpdateCategory from "./admin/category/updateCategory";
 // import { useParams } from "react-router-dom";/
@@ -47,16 +43,12 @@ function App() {
   const navigate = useNavigate();
   const [products, setProduct] = useState<IProduct[]>([]);
   const [categories, setCategory] = useState<ICate[]>([]);
-  // const [product, setProducts] = useState({});
 
-  // console.log(id);
   useEffect(() => {
     getAllProduct().then(({ data }) => setProduct(data));
     getAllCategory().then(({ data }) => setCategory(data));
   }, []);
 
-  // console.log(products);
-  ///SanPham
   const onHandleRemove = (id: number | string) => {
     deleteProduct(id)
       .then(() => {
@@ -167,16 +159,6 @@ function App() {
             />
           </Route>
         </Route>
-        <Route
-          path="/signup"
-          element={<SignUpPage onAddUser={onHandleAddUser} />}
-        />
-        <Route
-          path="/signin"
-          element={
-            <SignInPage onSignIn={onHandleSignin} onLogOut={onHandleLogOut} />
-          }
-        />
       </Routes>
     </div>
   );
